5fbe11cdde8705c44ecb5dcd5a1e85c4ee8941e5
Felipe Mariño <geduradev@gmail.com>
2023-01-09 02:14:40 -0500

    chore: change line to bootstrap imports

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap.js'
 import { createApp } from 'vue'
 import router from './router/router'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 
 createApp(App)
   .use(router())
