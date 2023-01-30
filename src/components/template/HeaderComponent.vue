405e0f723b03031c5ab240961ce677c290fd53e7
geduramc <geduradev@gmail.com>
2023-01-30 12:10:37 -0500

    fix: build script and move icons to public

--- src/components/template/HeaderComponent.vue
+++ src/components/template/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav class="navbar navbar-dark bg-dark fixed-top navbar-expand-lg">
+  <nav class="navbar bg-dark fixed-top navbar-expand-lg">
     <a class="navbar-brand header-icon" href="#">
-      <img src="@/assets/icons/geduramc_circle.png" width="30" height="30" alt="geduramc_icon">
+      <img src="@public/icons/geduramc_circle.png" width="30" height="30" alt="geduramc_icon">
     </a>
   </nav>
 </template>
@@ -22,5 +22,8 @@ export default {
 <style lang="css" scoped>
 a.header-icon {
   cursor: initial;
+}
+.bg-dark {
+  background-color: var(--background-primary) !important;
 }
 </style>
